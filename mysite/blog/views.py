8bc18b6bfaff666480f89e2d5362c70dd6bbb5fb
Ikechukwu Mbakwem <ikembakwem@gmail.com>
2023-02-28 09:49:42 +0100

    Implemented stemming and ranking in search results

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -6,7 +6,7 @@ from .forms import EmailPostForm, CommentForm, SearchForm
 from django.core.mail import send_mail
 from taggit.models import Tag
 from django.db.models import Count
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import SearchVector, SearchRank, SearchQuery
 
 
 class PostListView(ListView):
@@ -114,8 +114,15 @@ def post_search(request):
     form = SearchForm(request.GET)
     if form.is_valid():
       query = form.cleaned_data['query']
-      results = Post.published.annotate(search=SearchVector('title', 'body'),)\
-        .filter(search=query)
+      # results = Post.published.annotate(search=SearchVector('title', 'body'),)\
+      #   .filter(search=query)
+      # search_vector = SearchVector('title', 'body')
+      search_vector = SearchVector('title', weight='A') + SearchVector('body', weight='B')
+      search_query = SearchQuery(query)
+      search_rank = SearchRank(search_vector, search_query)
+      results = Post.published.annotate(search=search_vector, rank=search_rank)\
+            .filter(rank__gte=0.3).order_by('-rank')
+          # .filter(search=search_query).order_by('-rank')
 
   return render(request,
                 'blog/post/search.html',
